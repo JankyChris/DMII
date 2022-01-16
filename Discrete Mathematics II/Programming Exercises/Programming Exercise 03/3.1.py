d4f3988280fe51a3f1803f46c97ce15fd571bf44
“Christoph <chrissi@janky.de>
2022-01-16 09:55:35 +0100

    fixed wrong commit

--- Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
+++ Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
@@ -1,12 +1,32 @@
-from pyscipopt import Model
+from pyscipopt import Model, quicksum, multidict
+import csv
+import os
 
-model = Model("Diet")
+def file_paths():
+    """
+    setting the file paths
+    """
+    script_dir = os.path.dirname(__file__)
 
-x = model.addVar("x")
-y = model.addVar("y", vtype="INTEGER")
-model.setObjective(x + y)
-model.addCons(2*x - y*y >= 0)
-model.optimize()
-sol = model.getBestSol()
-print("x: {}".format(sol[x]))
-print("y: {}".format(sol[y]))
+    # setting the file path for allfoods.csv
+    allfoods_rel_path = "data/allfoods.csv"
+    allfoods_abs_file_path = os.path.join(script_dir, allfoods_rel_path)
+
+    # setting the file path for reference.csv
+    reference_rel_path = "data/reference.csv"
+    reference_abs_file_path = os.path.join(script_dir, reference_rel_path)
+
+    return allfoods_abs_file_path, reference_abs_file_path
+
+def import_csv(allfoods_abs_file_path, reference_abs_file_path):
+    with open(allfoods_abs_file_path, newline='') as csvfile:
+            allfoods = csv.DictReader(csvfile, delimiter=';')
+
+    with open(reference_abs_file_path, newline='') as csvfile:
+            reference = csv.DictReader(csvfile, delimiter=';')
+
+    return allfoods, reference
+
+if __name__ == "__main__":
+    allfoods_abs_file_path, reference_abs_file_path = file_paths()
+    allfoods, reference = import_csv(allfoods_abs_file_path, reference_abs_file_path)
