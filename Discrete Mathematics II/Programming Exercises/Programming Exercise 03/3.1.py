73b525f3cbed6e04ca1b579207e315482581840b
“Christoph <chrissi@janky.de>
2022-01-19 18:42:17 +0100

    added pandas csv reader and model definiton

--- Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
+++ Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
@@ -1,6 +1,11 @@
-from pyscipopt import Model, quicksum, multidict
-import csv
 import os
+import csv
+
+import numpy as np
+import pandas as pd
+
+from pyscipopt import Model, quicksum, multidict
+
 
 def file_paths():
     """
@@ -18,7 +23,7 @@ def file_paths():
 
     return allfoods_abs_file_path, reference_abs_file_path
 
-def import_csv(allfoods_abs_file_path, reference_abs_file_path):
+def import_csv(allfoods_abs_file_path: str, reference_abs_file_path: str):
     with open(allfoods_abs_file_path, newline='') as csvfile:
             allfoods = csv.DictReader(csvfile, delimiter=';')
 
@@ -27,6 +32,83 @@ def import_csv(allfoods_abs_file_path, reference_abs_file_path):
 
     return allfoods, reference
 
+def import_csv_pd(allfoods_abs_file_path: str, reference_abs_file_path: str):
+
+    # open csv and convert to dataframe
+    with open(allfoods_abs_file_path, newline='') as csvfile:
+        allfoods = pd.read_csv(csvfile, sep=";")
+
+    with open(reference_abs_file_path, newline='') as csvfile:
+        reference = pd.read_csv(csvfile, sep=";")
+
+    return allfoods, reference
+
+def create_dataset(foods, reference):
+
+    # TODO: reference.reindex([...])
+
+    F = foods["NDB_No"].count()
+    N = reference["Nutrient"].count()
+
+    a = np.zeros(N)
+    b = np.zeros(N)
+
+    for i in range(N):
+        a[i] = reference["Max"][i]
+        b[i] = reference["Min"][i]
+
+    n = np.zeros((F, N))
+
+    for j in range(F):
+        for i in range(N):
+            n[j][i] = 
+    return F, N, a, b, 0
+
+def diet_model(F,N,a,b,n):
+    """
+        - F         : set of foods
+        - N         : set of nutrients
+        - a[i]      : minimum intake of nutrient i
+        - b[i]      : maximum intake of nutrient i
+        - n[j][i]   : amount of nutrient i in food j
+    """
+
+    model = Model("Diet")
+
+    # define indices for fiber and sugar, respectively
+    i_fiber, i_sugar = 5, 6
+
+    # define objective vector c for the objective max c^T*x
+    def c(j):
+        return (n[i_fiber][j] - n[i_sugar][j])
+
+    # create variables
+    x = {}
+    for j in F:
+        x[j] = model.addVar(vtype="I", name="x(%s)"%j)
+
+    # define constraints:
+    for j in F:
+
+        # max amount of nutrient i
+        model.addCons(quicksum(n[j][i]*x[j] for i in N) <= b[j])
+
+        # min amount of nutrient i
+        model.addCons(quicksum(n[j][i]*x[j] for i in N) >= a[j])
+
+    # objective:
+    model.setObjective(quicksum((c[j]*x[j]) for j in F), "maximize")
+    model.data = x
+
+    return model
+
+    
 if __name__ == "__main__":
     allfoods_abs_file_path, reference_abs_file_path = file_paths()
-    allfoods, reference = import_csv(allfoods_abs_file_path, reference_abs_file_path)
+    allfoods, reference = import_csv_pd(allfoods_abs_file_path, reference_abs_file_path)
+
+    F, N, a, b, n = create_dataset(allfoods, reference)
+
+    #model = diet_model(F, N, a, b, n)
+    # model.hideOutput() # silent mode
+    #model.optimize()
