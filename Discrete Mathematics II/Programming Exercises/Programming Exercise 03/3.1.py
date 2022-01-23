270fc22fdea28212e6a7f48abf53b2361007aa6c
“Christoph <chrissi@janky.de>
2022-01-23 15:14:22 +0100

    added function "data_cleanup"

--- Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
+++ Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
@@ -1,3 +1,4 @@
+from cmath import nan
 import os
 import csv
 
@@ -9,7 +10,7 @@ from pyscipopt import Model, quicksum, multidict
 
 def file_paths():
     """
-    setting the file paths
+    Sets the file paths
     """
     script_dir = os.path.dirname(__file__)
 
@@ -33,8 +34,9 @@ def import_csv(allfoods_abs_file_path: str, reference_abs_file_path: str):
     return allfoods, reference
 
 def import_csv_pd(allfoods_abs_file_path: str, reference_abs_file_path: str):
-
-    # open csv and convert to dataframe
+    """
+    Opens csv and converts to Pandas DataFrame
+    """
     with open(allfoods_abs_file_path, newline='') as csvfile:
         allfoods = pd.read_csv(csvfile, sep=";")
 
@@ -43,9 +45,25 @@ def import_csv_pd(allfoods_abs_file_path: str, reference_abs_file_path: str):
 
     return allfoods, reference
 
-def create_dataset(foods, reference):
+def create_dataset(foods: pd.DataFrame, reference: pd.DataFrame):
+    """
+    Creates datasets for further use in the model
+    """
+    def data_cleanup(foods: pd.DataFrame, reference: pd.DataFrame):
+        """
+        Performs cleanup of foods, reference s. t. layout matches for further computation
+        """
 
-    # TODO: reference.reindex([...])
+        # add rows "Water", "Total Sugar"
+        reference.loc[30] = (["Water", "g", nan, nan])
+        reference.loc[31] = (["Total Sugar", "g", nan, nan])
+
+        # reorder rows to match "allfoods.csv"
+        reference = reference.reindex([30, 0, 2, 1, 6, 7, 31, 22, 25, 24, 23, 21, 20, 26, 28, 29, 27, 19, 12, 13, 14, 17, 15, 16, 18, 8, 10, 9, 11, 3, 4, 5]).reset_index()
+
+        return foods, reference
+
+    foods, reference = data_cleanup(foods, reference)
 
     F = foods["NDB_No"].count()
     N = reference["Nutrient"].count()
@@ -57,17 +75,14 @@ def create_dataset(foods, reference):
         a[i] = reference["Max"][i]
         b[i] = reference["Min"][i]
 
-    n = np.zeros((F, N))
+    n = np.delete(foods.to_numpy(), [0, 1], 1)
 
-    for j in range(F):
-        for i in range(N):
-            n[j][i] = 
-    return F, N, a, b, 0
+    return F, N, a, b, n
 
 def diet_model(F,N,a,b,n):
     """
-        - F         : set of foods
-        - N         : set of nutrients
+        - F         : number of foods
+        - N         : number of nutrients
         - a[i]      : minimum intake of nutrient i
         - b[i]      : maximum intake of nutrient i
         - n[j][i]   : amount of nutrient i in food j
@@ -84,20 +99,20 @@ def diet_model(F,N,a,b,n):
 
     # create variables
     x = {}
-    for j in F:
+    for j in range(F):
         x[j] = model.addVar(vtype="I", name="x(%s)"%j)
 
     # define constraints:
-    for j in F:
-
+    for j in range(F):
+        
         # max amount of nutrient i
-        model.addCons(quicksum(n[j][i]*x[j] for i in N) <= b[j])
+        model.addCons(quicksum(n[j][i]*x[j] for i in range(N)) <= b[j])
 
         # min amount of nutrient i
-        model.addCons(quicksum(n[j][i]*x[j] for i in N) >= a[j])
+        model.addCons(quicksum(n[j][i]*x[j] for i in range(N)) >= a[j])
 
     # objective:
-    model.setObjective(quicksum((c[j]*x[j]) for j in F), "maximize")
+    model.setObjective(quicksum((c[j]*x[j]) for j in range(F)), "maximize")
     model.data = x
 
     return model
@@ -109,6 +124,6 @@ if __name__ == "__main__":
 
     F, N, a, b, n = create_dataset(allfoods, reference)
 
-    #model = diet_model(F, N, a, b, n)
-    # model.hideOutput() # silent mode
-    #model.optimize()
+    model = diet_model(F, N, a, b, n)
+    model.hideOutput() # silent mode
+    model.optimize()
