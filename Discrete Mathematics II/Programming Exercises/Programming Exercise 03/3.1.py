c47893428343a887f83953aa11fb72e5ef4fb6db
“Christoph <chrissi@janky.de>
2022-02-01 17:43:26 +0100

    final

--- Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
+++ Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.1.py
@@ -1,4 +1,19 @@
-from cmath import nan
+"""
+This program computes the RCSPP as stated in
+Programming Exercise 3.1
+
+File system should be setup as follows:
+
+├── data/
+│   ├── allfoods.csv
+│   └── reference.csv
+└── 3.1.py
+
+Group: Dallmer, Kane, Jankowsky
+
+"""
+
+
 import os
 import csv
 
@@ -24,7 +39,12 @@ def file_paths():
 
     return allfoods_abs_file_path, reference_abs_file_path
 
+
 def import_csv(allfoods_abs_file_path: str, reference_abs_file_path: str):
+    """
+    Imports csv-files using "csv"-package
+    (not used)
+    """
     with open(allfoods_abs_file_path, newline='') as csvfile:
             allfoods = csv.DictReader(csvfile, delimiter=';')
 
@@ -32,6 +52,7 @@ def import_csv(allfoods_abs_file_path: str, reference_abs_file_path: str):
             reference = csv.DictReader(csvfile, delimiter=';')
 
     return allfoods, reference
+
 
 def import_csv_pd(allfoods_abs_file_path: str, reference_abs_file_path: str):
     """
@@ -45,21 +66,54 @@ def import_csv_pd(allfoods_abs_file_path: str, reference_abs_file_path: str):
 
     return allfoods, reference
 
+
 def create_dataset(foods: pd.DataFrame, reference: pd.DataFrame):
     """
     Creates datasets for further use in the model
     """
     def data_cleanup(foods: pd.DataFrame, reference: pd.DataFrame):
         """
-        Performs cleanup of foods, reference s. t. layout matches for further computation
+        Performs cleanup of foods, reference
+        s. t. layout matches for further computation
         """
 
         # add rows "Water", "Total Sugar"
         reference.loc[30] = (["Water", "g", None, None])
         reference.loc[31] = (["Total Sugar", "g", None, None])
-        
+
         # reorder rows to match "allfoods.csv"
-        reference = reference.reindex([30, 0, 2, 1, 6, 7, 31, 22, 25, 24, 23, 21, 20, 26, 28, 29, 27, 19, 12, 13, 14, 17, 15, 16, 18, 8, 10, 9, 11, 3, 4, 5]).reset_index()
+        reference = reference.reindex([30,
+                                        0,
+                                        2,
+                                        1,
+                                        6,
+                                        7,
+                                       31,
+                                       22,
+                                       25,
+                                       24,
+                                       23,
+                                       21,
+                                       20,
+                                       26,
+                                       28,
+                                       29,
+                                       27,
+                                       19,
+                                       12,
+                                       13,
+                                       14,
+                                       17,
+                                       15,
+                                       16,
+                                       18,
+                                        8,
+                                       10,
+                                        9,
+                                       11,
+                                        3,
+                                        4,
+                                        5]).reset_index()
 
         return foods, reference
 
@@ -68,31 +122,36 @@ def create_dataset(foods: pd.DataFrame, reference: pd.DataFrame):
     F = foods["NDB_No"].count()
     N = reference["Nutrient"].count()
 
-    a = np.zeros(N)
-    b = np.zeros(N)
+    mn = np.zeros(N)
+    mx = np.zeros(N)
 
     for i in range(N):
-        a[i] = reference["Min"][i]
-        b[i] = reference["Max"][i]
+        mn[i] = reference["Min"][i]
+        mx[i] = reference["Max"][i]
 
     n = np.delete(foods.to_numpy(), [0, 1], 1)
 
-    print(b)
+    # check if nutrients are valid
+    for j, i in np.ndindex(n.shape):
+        if n[j, i] < 0:
+            raise ValueError("Food ["+foods["Shrt_Desc"][j]
+                             + "] contains negative amount of ["
+                             + reference["Nutrient"][i] + "]!")
 
-    return F, N, a, b, n
+    return mn, mx, n
 
-def diet_model(F,N,a,b,n):
+
+def diet_model(mn, mx, n):
     """
-        - F         : number of foods
-        - N         : number of nutrients
-        - a[i]      : minimum intake of nutrient i
-        - b[i]      : maximum intake of nutrient i
-        - n[j][i]   : amount of nutrient i in food j
+    Defines the LP-model with parameters:
+    - mx[i]      : minimum intake of nutrient i
+    - mx[i]      : maximum intake of nutrient i
+    - n[j][i]   : amount of nutrient i in food j
     """
 
     model = Model("Diet")
 
-    # define indices for fiber and sugar, respectively
+    (F, N) = len(n), 32
     i_fiber, i_sugar = 5, 6
 
     # define objective vector c for the objective [ max c^T*x ]
@@ -104,32 +163,49 @@ def diet_model(F,N,a,b,n):
     # create variables
     x = {}
     for j in range(F):
-        x[j] = model.addVar(vtype="C", name="x(%s)"%j)
+        x[j] = model.addVar(vtype="C")
 
     # define constraints:
     cons = {}
     for i in range(N):
-        cons[i] = model.addCons(quicksum(n[j][i]*x[j] for j in range(F)) <= b[i], name="Nutr(%s)"%i)
-        model.chgLhs(cons[i], a[i])
+        cons[i] = model.addCons(quicksum(n[j][i]*x[j] for j in range(F))
+                                <= mx[i])   # upper constraint
+        model.chgLhs(cons[i], mn[i])        # lower constraint
 
     # objective:
-    model.setObjective(quicksum((fiber[j]*x[j] - sugar[j]*x[j]) for j in range(F)), "maximize")
+    model.setObjective(quicksum((fiber[j]*x[j]
+                                - sugar[j]*x[j]) for j in range(F)),
+                                "minimize")
     model.data = x
 
     return model
 
-    
+
 if __name__ == "__main__":
     allfoods_abs_file_path, reference_abs_file_path = file_paths()
-    allfoods, reference = import_csv_pd(allfoods_abs_file_path, reference_abs_file_path)
+    allfoods, reference = import_csv_pd(allfoods_abs_file_path,
+                                        reference_abs_file_path)
 
-    F, N, a, b, n = create_dataset(allfoods, reference)
+    mn, mx, n = create_dataset(allfoods, reference)
 
-    model = diet_model(F, N, a, b, n)
-
-    #model.hideOutput() # silent mode
+    model = diet_model(mn, mx, n)
+    model.hideOutput()
     model.optimize()
 
-    #x = model.data
-    #for j in x:
-    #    print(model.getVal(x[j]))
+    if (model.getStatus() == "infeasible"):
+        raise AssertionError("The model is infeasible!")
+
+    name = {}
+    amount = {}
+    x = model.data
+    i = 0
+
+    for j in x:
+        if model.getVal(x[j]) != 0:
+            name[j] = allfoods["Shrt_Desc"][i]
+            amount[j] = model.getVal(x[j])
+            i += 1
+
+    print("Today I will eat this:")
+    for food, amnt in zip(name, amount):
+        print(food, ":", amnt/100.0, "g")
--- Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.2.py
+++ Discrete Mathematics II/Programming Exercises/Programming Exercise 03/3.2.py
@@ -1,11 +1,34 @@
-import os, time
+"""
+This program computes the RCSPP as stated in
+Programming Exercise 3.2
+
+File system should be setup as follows:
+
+├── data/
+│   ├── graph1_2.txt
+│   ├── graph4_2.txt
+│   ├── grid1.txt
+│   └── grid2.txt
+├── DiGraph.py
+└── 3.2.py
+
+Group: Dallmer, Kane, Jankowsky
+
+"""
+
+import os
+import time
 
 from pyscipopt import Model, quicksum
 
 from DiGraph import DiGraph
 
+
 def rcspp(G, s, t, l):
     """
+    Defines the model for the Resource Constrained Shortest Path Problem
+    with parameters:
+
     G       :   Graph
     s       :   starting node
     t       :   end node
@@ -27,11 +50,14 @@ def rcspp(G, s, t, l):
 
     """define contraints"""
     # chosen path is st-path:
-    model.addCons( ( quicksum(x[a] for a in out_arcs[s]) - quicksum(x[a] for a in in_arcs[s]) ) == 1)
-    model.addCons( ( quicksum(x[a] for a in out_arcs[t]) - quicksum(x[a] for a in in_arcs[t]) ) == -1)
+    model.addCons((quicksum(x[a] for a in out_arcs[s])
+                   - quicksum(x[a] for a in in_arcs[s])) == 1)
+    model.addCons((quicksum(x[a] for a in out_arcs[t])
+                   - quicksum(x[a] for a in in_arcs[t])) == -1)
     for u in vertices:
         if u != s and u != t:
-            model.addCons( ( quicksum(x[a] for a in out_arcs[u]) - quicksum(x[a] for a in in_arcs[u]) ) == 0)
+            model.addCons((quicksum(x[a] for a in out_arcs[u])
+                           - quicksum(x[a] for a in in_arcs[u])) == 0)
     # resource constraint:
     model.addCons(quicksum(r[a]*x[a] for a in arcs) <= l)
 
@@ -41,12 +67,14 @@ def rcspp(G, s, t, l):
     model.data = x, r
     return model
 
+
 def create_path(x, start):
     """
-    unction that converts the model values to a path array
+    Creates a feasible path from the output of
+    the SCIP optimization in the form of an array
     """
 
-    arcs =[]
+    arcs = []
 
     for j in x:
         if model.getVal(x[j]) == 1:
@@ -63,17 +91,25 @@ def create_path(x, start):
                 pass
 
     return path
-    
+
+
 def consumption(model, G, x, r):
+    """
+    Returns the cost of the RCSP
+    """
     arcs = G.arcs()
     return int((sum(r[a]*model.getVal(x[a]) for a in arcs)))
+
 
 if __name__ == "__main__":
     term_size = os.get_terminal_size()
 
     dirname = os.path.dirname(__file__)
-    instances = ["data/graph1_2.txt", "data/graph4_2.txt", "data/grid1.txt", "data/grid2.txt"]
-    #instances = ["data/graph4_2.txt"]
+    instances = ["data/graph1_2.txt",
+                 "data/graph4_2.txt",
+                 "data/grid1.txt",
+                 "data/grid2.txt"]
+    # instances = ["data/graph4_2.txt"]
     params = {
             "data/graph1_2.txt": [1, 8, 11],
             "data/graph4_2.txt": [7743, 5983, 280],
@@ -93,7 +129,7 @@ if __name__ == "__main__":
         s, t, l = params[inst][0], params[inst][1], params[inst][2]
 
         model = rcspp(G, s, t, l)
-        model.hideOutput() # silent mode
+        model.hideOutput()  # silent mode
         model.optimize()
 
         solvetime = model.getSolvingTime()
@@ -105,6 +141,8 @@ if __name__ == "__main__":
 
         runtime = time.process_time() - start
 
-        print("\nA shortest "+str(s)+"-"+str(t)+"-path with resource consumption ≤", l, "is\n", path)
+        print("\nA shortest " + str(s) + "-" + str(t)
+              + "-path with resource consumption ≤", l, "is\n", path)
         print("with length", opt_val, "and resource consumption", cons, ".")
-        print("The computation took ", "%.1f" % runtime, "seconds of which", "%.1f" % solvetime, "seconds were used to solve the model.")
+        print("The computation took ", "%.1f" % runtime, "seconds of which",
+              "%.1f" % solvetime, "seconds were used to solve the model.")
